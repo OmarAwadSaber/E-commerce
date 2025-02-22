df15fd58cf9d34abcd0b53c7fff8d185e1dd9ce2
Omar Awad <154761782+OmarAwadSaber@users.noreply.github.com>
2025-02-22 18:25:05 +0200

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
